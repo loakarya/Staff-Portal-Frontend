6fb03c0f1abe95d9a6b581c117fe0b2c4f1a45da
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2021-04-22 11:49:15 +0700

    [Update] Reorganize login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
@@ -9,12 +8,12 @@ import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Snackbar from "@material-ui/core/Snackbar";
 import { makeStyles } from "@material-ui/core/styles";
 
+import BusinessmanImage from "../assets/businessman.jpg";
 import GoogleLogo from "../assets/google_logo.png";
 import LoakaryaLogo from "../assets/logo.png";
 
@@ -41,7 +40,7 @@ const useStyles = makeStyles((theme) => ({
     height: "100vh",
   },
   image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundImage: `url(${BusinessmanImage})`,
     backgroundRepeat: "no-repeat",
     backgroundColor: theme.palette.grey[50],
     backgroundSize: "cover",
